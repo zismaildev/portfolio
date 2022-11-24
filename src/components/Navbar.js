51f6ae2701623f7a1aebfb6c0350ac770cfb8990
Zismail-Dev <nattapong130247@gmail.com>
2022-11-25 03:31:38 +0700

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -23,7 +23,7 @@ export default function NewNav() {
                             height="30"
                             className="d-inline-block align-top"
                         />{' '}
-                        Zismail-Dev
+                        P.Nattapong
                     </Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
