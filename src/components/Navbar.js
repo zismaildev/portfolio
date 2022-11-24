87b00b87a26140d9ac47e5a9221883e0ee4c5ccc
Zismail-Dev <nattapong130247@gmail.com>
2022-11-25 03:48:53 +0700

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -30,10 +30,10 @@ export default function NewNav() {
                         <Nav className="me-auto">
                         </Nav>
                         <Nav className="mr-auto">
-                        <Nav.Link href="/portfolio">Portfolio</Nav.Link>
-                            <Nav.Link href="/contact">Contact</Nav.Link>
-                            <Nav.Link href="/about">About</Nav.Link>
-                            <Nav.Link href="/educaction">Educaction</Nav.Link>
+                        <Nav.Link href="#/portfolio">Portfolio</Nav.Link>
+                            <Nav.Link href="#/contact">Contact</Nav.Link>
+                            <Nav.Link href="#/about">About</Nav.Link>
+                            <Nav.Link href="#/educaction">Educaction</Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
